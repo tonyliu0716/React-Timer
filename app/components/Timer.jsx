41c485e97331c253303867e777ecaf575ee17813
tonyliu0716 <tonyliu0716@gmail.com>
2018-12-29 18:18:16 -0600

    add router and navigation style

---
+++ app/components/Timer.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var Timer = React.createClass({
+    render: function() {
+        return (
+            <div>
+                This is the Timer.jsx
+            </div>
+
+        );
+    }
+});
+
+module.exports = Timer;
---
+++ app/tests/app.test.jsx
@@ -0,0 +1,10 @@
+// karma.conf.js would read this file
+
+var expect = require('expect');
+
+describe('App', () => {
+   it('should properly run tests', () => {
+      expect(1).toBe(1);
+   });
+
+});
